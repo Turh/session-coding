ceefc78d7ce3a4c058d73c3b82b38460409b8acd
Uri Ops <cat12345@gmail.com>
2016-10-16 12:40:29 +0300

    answered operators.rb practice

--- home-assignments/session-3/operators.rb
+++ home-assignments/session-3/operators.rb
@@ -1,10 +1,31 @@
 #fill in the boolean values
 
 # boolean_1 = 77 < 78 && 77 < 77
-boolean_1 = 
+#boolean_1 = TRUE & FALSE = FALSE
+# Final Answer = >> boolean_1 = FALSE <<
+#if ( 77 < 78 && 77 < 77 )
+#	puts "TRUE"
+#else
+#	puts "FALSE"
+#end
+
 
 # boolean_2 = true && 100 >= 100
-boolean_2 = 
+#boolean_2 = TRUE & TRUE = TRUE
+# Final Answer = >> boolean_2 = TRUE <<
+#if ( true && 100 >= 100 )
+#	        puts "TRUE"
+#else
+#	        puts "FALSE"
+#end
+
 
 # boolean_3 = 2**3 == 8 && 3**2 == 9
-boolean_3 = 
+#boolean_3 = TRUE & TRUE = TRUE
+# Final Answer = >> boolean_3 = TRUE <<
+#if ( 2**3 == 8 && 3**2 == 9 )
+# 	       puts "TRUE"
+#else
+#               puts "FALSE"
+#end
+
